efb3a54675240bbda8d1e479365d09a44aafdd63
Prakash Rai <prai@mum.edu>
2018-03-20 12:48:05 -0500

    V2

--- Template/js/wo.js
+++ Template/js/wo.js
@@ -11,4 +11,42 @@ $(document).ready(function(){
         let postId = $(this).attr('post-id');
         $('#post-comment-' + postId).toggle(200);
     });
+
+    //Changing profile picture
+    $(".upload-profile-picture").click(function () {
+        $("#profile-picture").trigger('click');
+    });
+
+    //change image on leselection
+    $("#profile-picture").change(function(){
+        let input = this;
+        if (input.files) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.upload-profile-picture')
+                    .attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+
+    //Changing Event picture
+    $(".upload-event-picture").click(function () {
+        $("#event-picture").trigger('click');
+    });
+
+    //change image on leselection
+    $("#event-picture").change(function(){
+        let input = this;
+        if (input.files) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.upload-event-picture')
+                    .attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
 });
