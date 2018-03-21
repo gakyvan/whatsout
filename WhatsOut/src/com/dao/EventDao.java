b2ac03fe1707a2c46d9de7098349d01eba19f03e
Yvan GAKUBA <Yvan GAKUBA@172.19.141.169>
2018-03-20 20:10:29 -0500

    20.03.2018 20:10

--- WhatsOut/src/com/dao/EventDao.java
+++ WhatsOut/src/com/dao/EventDao.java
@@ -30,4 +30,25 @@ public class EventDao {
 		qex.closeConnection();
 		return result;
 	}
+	
+	/*
+	 * Written on March 20, 2018 
+	 * This function receives an event object and creates an event in the database;
+	 * Uses the model written by Rupendre Maharjan
+	 * Uses the QueryExecutor Class written by Yvan GAKUBA
+	 * @Author Yvan GAKUBA
+	 * */
+	public boolean update(Event event) {
+		QueryExecutor qex = new QueryExecutor();
+		String query = "UPDATE events "
+				+ "(title,description,logo,createdOn,happeningOn,endingOn,capacity,addressId,categoryId,ownerID)"
+				+ "VALUES(?,?,?,?,?,?,?,?,?,?)";
+		boolean result = qex.insert(query,event.getTitle(),event.getDescription(),event.getLogo(),event.getCreatedOn().toString(),
+				event.getHappeningOn().toString(),event.getEndingOn().toString(),event.getCapacity(),event.getAddress().getId(),
+				event.getEventCategory().getId(),event.getEventCreator().getId());
+		qex.closeConnection();
+		return result;
+	}
+	
+	
 }
